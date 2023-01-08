a27d6c775ebbab22887da055e710bdaab1f8cde1
tomasyovino <tomasyovino@gmail.com>
2023-01-08 13:48:44 -0300

    Add stripe & cors modules

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import router from "./src/routes/index.js";
 import config from "./src/utils/config.js";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/api", router);
 
--- src/utils/config.js
+++ src/utils/config.js
@@ -5,7 +5,8 @@ dotenv.config();
 const config = {
     PORT: process.env.PORT,
     mongo_uri: process.env.MONGO_URI,
-    jwt_sec: process.env.JWT_SECRET
+    jwt_sec: process.env.JWT_SECRET,
+    stripe_key: process.env.STRIPE_KEY
 };
 
 export default config;
