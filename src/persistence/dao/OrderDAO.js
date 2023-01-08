7e2d20a90283dd84c1e61e8da90eb5f656b0d0a6
tomasyovino <tomasyovino@gmail.com>
2023-01-07 21:28:55 -0300

    Display order CRUD operations with JWT

--- src/persistence/dao/OrderDAO.js
+++ src/persistence/dao/OrderDAO.js
@@ -14,6 +14,48 @@ class OrderDAO extends DAOContainer {
         };
         return instance;
     };
+
+    async createOrder(obj) {
+        try {
+            const newOrder = new OrderModel(obj);
+            const savedOrder = await newOrder.save();
+            return savedOrder;
+        } catch (err) {
+            console.log(err)
+        }
+    };
+
+    async findOrdersByUserId(userId) {
+        try {
+            const orders = await OrderModel.find({ userId });
+            return orders;
+        } catch (err) {
+            console.log(err)
+        };
+    };
+
+    async getOrdersStats(previousMonth) {
+        try {
+            const data = await OrderModel.aggregate([
+                { $match: { createdAt: { $gte: previousMonth } } },
+                {
+                    $project: {
+                        month: { $month: "$createdAt" },
+                        sales: "$amount"
+                    },
+                },
+                {
+                    $group: {
+                        _id: "$month",
+                        total: { $sum: "$sales" },
+                    },
+                },
+            ]);
+            return data;
+        } catch (err) {
+            console.log(err)
+        };
+    };
 };
 
 export default OrderDAO;
--- src/routes/order.js
+++ src/routes/order.js
@@ -1,5 +1,65 @@
 import { Router } from "express";
+import { verifyToken, verifyTokenAndAdmin, verifyTokenAndAuthorization } from "../middlewares/verifyToken.js";
+import { findAllOrdersController, findOrdersByUserIdController, getOrdersStatsController, createOrderController, updateOrderByIdController, deleteOrderByIdController } from "../controllers/order.controllers.js";
 
 const orderRouter = Router();
+
+orderRouter.get("/", verifyTokenAndAdmin, async (req, res) => {
+    try {
+        const orders = await findAllOrdersController(false);
+        res.status(200).json(orders);
+    } catch (err) {
+        res.status(500).json(err);
+    };
+});
+
+orderRouter.get("/find/:userId", verifyTokenAndAuthorization, async (req, res) => {
+    try {
+        const orders = await findOrdersByUserIdController(req.params.userId);
+        res.status(200).json(orders);
+    } catch (err) {
+        res.status(500).json(err);
+    };
+});
+
+orderRouter.get("/income", verifyTokenAndAdmin, async (req, res) => {
+    try {
+        const date = new Date();
+        const lastMonth = new Date(date.setMonth(date.getMonth() - 1));
+        const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth() - 1));
+        
+        const data = await getOrdersStatsController(previousMonth);
+        res.status(200).json(data);
+    } catch (err) {
+        res.status(500).json(err);
+    };
+});
+
+orderRouter.post("/", verifyToken, async (req, res) => {
+    try {
+        const order = await createOrderController(req.body);
+        res.status(200).json(order);
+    } catch (err) {
+        res.status(500).json(err);
+    };
+});
+
+orderRouter.put("/:id", verifyTokenAndAdmin, async (req, res) => {
+    try {
+        const order = await updateOrderByIdController(req.params.id, req.body);
+        res.status(200).json(order);
+    } catch (err) {
+        res.status(500).json(err);
+    };
+});
+
+orderRouter.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
+    try {
+        const deletedOrder = await deleteOrderByIdController(req.params.id);
+        res.status(200).json({ message: "The order has been deleted...", deletedOrder });
+    } catch (err) {
+        res.status(500).json(err);
+    };
+});
 
 export default orderRouter;
